5c46dea3105a5423d5ad0385aa79a2e2f133eb3a
cmoraes <cmoraes@bmc.com>
2021-11-23 12:39:33 +0200

    fix client dependencies

--- src/clients/ctm_api_client/api_client.py
+++ src/clients/ctm_api_client/api_client.py
@@ -282,7 +282,7 @@ class ApiClient(object):
             if klass in self.NATIVE_TYPES_MAPPING:
                 klass = self.NATIVE_TYPES_MAPPING[klass]
             else:
-                klass = getattr(ctm_api_client.models, klass)
+                klass = getattr(clients.ctm_api_client.models, klass)
 
         if klass in self.PRIMITIVE_TYPES:
             return self.__deserialize_primitive(data, klass)
