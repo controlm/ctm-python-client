f2529a5a2c54a4394ac05f312ead1d3d3276dbeb
cmoraes <cmoraes@bmc.com>
2021-11-30 21:16:02 +0200

    remove outdated docker files

--- examples/python_notebooks/StartDocker.sh
+++
@@ -1,3 +0,0 @@
-sudo docker build -t docker_ctm_python .
-docker run --rm -p 8787:8787 -v $(PWD):/opt/notebooks docker_ctm_python /bin/bash -c \
-"jupyter notebook --ip=0.0.0.0 --port=8787 --notebook-dir=/opt/notebooks --allow-root --no-browser"
