0053e3893ca4b6ed5cc3754bbcf2ea464b0c404c
Tijs.Monté <tijs_monte@bmc.com>
2021-12-22 21:34:41 +0100

    Changed version to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="ctm-python-client",
     packages=find_packages(where="src"),    
     package_dir={"": "src"},
-    version="1.0.0",
+    version="1.0.1",
     description="Python Workflows for Control-M",
     author="BMC Software",
     license="BSD 2-Clause",
