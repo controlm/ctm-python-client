57dc0e8c62d722b41fc66b2110ba5486bc888fef
cmoraes <celino_moraes@bmc.com>
2022-07-24 14:49:25 +0300

    + refactor version info

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
+import sys
+sys.path.append('src')
+import ctm_python_client
 
 setup(
     name="ctm-python-client",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    version="2.0.0",
+    version=ctm_python_client.__version__,
     description="Python Workflows for Control-M",
-    author="BMC Software",
+    author=ctm_python_client.__author__,
     license="BSD 3-Clause",
     install_requires=["requests>=2.23.0",
                       "urllib3", "six", "attrs", "certifi"],
