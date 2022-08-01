1cafef594e1cbaa7753a41cf47c2aab23e9e30d3
cmoraes <celino_moraes@bmc.com>
2022-08-01 14:42:36 +0300

    + ut for json aapi

--- tests/test_aapi.py
+++ tests/test_aapi.py
@@ -7,5 +7,25 @@ def test_json_generation():
     folder.job_list.append(aapi.JobCommand('JC1', command='ls -l'))
     folder.job_list.append(aapi.JobAWSLambda('JAL1', connection_profile='CPAWS', function_name='fname'))    
 
-    o = json.loads(folder.dumps_aapi(indent=2))
+    o = json.loads(folder.dumps_aapi(indent=2))        
+    assert o == folder.as_aapi_dict()
+
+
+    o = json.loads('''
+    {
+        "Type": "Folder",
+        "JC1": {
+            "Type": "Job:Command",
+            "Command": "ls -l"
+        },
+        "JAL1": {
+            "Type": "Job:AWS:Lambda",
+            "ConnectionProfile": "CPAWS",
+            "FunctionName": "fname"
+        },
+        "RunAs": "controlm"
+
+    }
+    ''')
+
     assert o == folder.as_aapi_dict()
