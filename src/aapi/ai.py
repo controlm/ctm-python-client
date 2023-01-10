0c4c4e3b272c44ee9cfd7d95f5b372255b590423
cmoraes <celino_moraes@bmc.com>
2023-01-10 10:24:27 +0200

    + ai connection profile

--- src/aapi/ai.py
+++ src/aapi/ai.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from aapi.job import Job
+from aapi.connectionprofile import ConnectionProfile
 
 import attrs
 
@@ -14,21 +15,49 @@ class AIJob(Job):
         metadata={'_aapi_repr_': 'ConnectionProfile'})
 
     @staticmethod
-    def field_optional(name: str):
+    def field_optional(name: str, default: str=None):
         if not name.startswith('AI-'):
             name = 'AI-'+name
 
-        return attrs.field(metadata={'_aapi_repr_': name}, kw_only=True, default=None)
+        return attrs.field(metadata={'_aapi_repr_': name}, kw_only=True, default=default)
 
     @staticmethod
     def field(name: str):
         if not name.startswith('AI-'):
             name = 'AI-'+name
 
-        return attrs.field(metadata={'_aapi_repr_': name})        
+        return attrs.field(metadata={'_aapi_repr_': name})
 
     @staticmethod
     def type_field(name: str):
-        name = 'Job:ApplicationIntegrator:'+name
+        name = 'Job:ApplicationIntegrator:AI '+name
         return attrs.field(init=False, default=name, metadata={
             '_aapi_repr_': 'Type', '_type_aapi_': name})
+
+
+@attrs.define
+class AIConnectionProfile(ConnectionProfile):
+    _type: str = attrs.field(init=False, default='AI ConnectionProfile', metadata={
+                             '_aapi_repr_': 'Type', '_type_aapi_': 'AI ConnectionProfile'})
+
+    object_name: str = attrs.field(metadata={'_aapi_name_': True})
+
+    @staticmethod
+    def type_field(name: str):
+        name = 'ConnectionProfile:ApplicationIntegrator:AI '+name
+        return attrs.field(init=False, default=name, metadata={
+            '_aapi_repr_': 'Type', '_type_aapi_': name})
+
+
+    @staticmethod
+    def field_optional(name: str, default:str = None):
+        if not name.startswith('AI-'):
+            name = 'AI-'+name
+
+        return attrs.field(metadata={'_aapi_repr_': name}, kw_only=True, default=default)
+
+    @staticmethod
+    def field(name: str):
+        if not name.startswith('AI-'):
+            name = 'AI-'+name
+        return attrs.field(metadata={'_aapi_repr_': name})
