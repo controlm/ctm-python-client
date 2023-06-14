05388419afb3c93005c30953216d8394af72af18
Refael Beker <134957947+rbeker-BMC@users.noreply.github.com>
2023-06-14 16:23:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,15 @@ setup(
     package_dir={"": "src"},
     version="2.1.2",
     description="Python Workflows for Control-M",
+    long_description="Python Workflows for Control-M",
     author="BMC Software",
     license="BSD 3-Clause",
+    keywords="Control-M",
     install_requires=["requests>=2.23.0",
                       "urllib3", "six", "attrs", "certifi", "jinja2"],
+    classifiers= [
+        "DEVELOPMENT STATUS :: 5 - PRODUCTION/STABLE",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ]
 )
