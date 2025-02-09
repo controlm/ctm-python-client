a782df6008e61166708b53eb3a5c999f2400fc3f
shacharl <shachar_lancer@bmc.il>
2025-02-09 10:58:31 +0200

    Control-M Integrations Factory up to January 2025

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="ctm-python-client",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    version="2.4.5",
+    version="2.4.6",
     description="Python Workflows for Control-M",
     long_description=long_description,
     long_description_content_type='text/markdown',
